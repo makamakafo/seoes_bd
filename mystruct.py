4c344fbd8699d85109214e2a479ffdf10d5f824d
Лиана Макаримова <ledili2004@gmail.com>
2024-12-03 14:08:55 +0300

    complete

--- mystruct.py
+++ mystruct.py
@@ -1,4 +1,5 @@
 import re
+import random as rnd
 
 
 class FDataBase:
@@ -11,8 +12,6 @@ class FDataBase:
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
-# Эта строка выводит результат в консоль, но обычно ее лучше убрать в продакшне
-            print(res)
             return res
         except Exception:
             print("Ошибка чтения из БД")
@@ -20,15 +19,111 @@ class FDataBase:
 
     def create_user(self, role, name, number):
         try:
-            self.__cur.execute("call insert_seoes_users(%d, %s, %s)"[
-                                                    role, name, number])
+            self.__cur.execute("call insert_seoes_users(%s, %s, %s)", (
+                                                    int(role), name, number))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка добавления записи: {e}")
+            return False
+        return True
+
+    def delete_user(self, user_id):
+        try:
+            id = str(user_id)
+            self.__cur.execute("call delete_seoes_users(%s)", (id,))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка удаления записи: {e}")
+            return False
+        return True
+    
+    def get_memberships(self):
+        sql = "SELECT * FROM user_group_memberships"
+        try:
+            self.__cur.execute(sql)
             res = self.__cur.fetchall()
-# Эта строка выводит результат в консоль, но обычно ее лучше убрать в продакшне
+            return res
+        except Exception:
+            print("Ошибка чтения из БД")
+            return []
+        
+    def get_configs(self):
+        try:
+            self.__cur.execute('select * from seoes_config_view order by "ID Конфига"')
+            res = self.__cur.fetchall()
+            return res
+        except Exception:
+            print("Ошибка чтения из БД")
+            return []
+        
+    def get_search_data(self, id):
+        try:
+            self.__cur.execute("select * from get_config_search_data(%s)", (id,))
+            res = self.__cur.fetchall()
             print(res)
             return res
         except Exception:
-            print("Ошибка добавления записи")
+            print("Ошибка чтения из БД")
             return []
+        
+    def create_config(self, group_id, host_user_id, site_name, url, access_token):
+        try:
+            self.__cur.execute("call insert_seoes_config(%s, %s, %s, %s, %s)", (
+                int(group_id), int(host_user_id), site_name, url, access_token))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка добавления записи: {e}")
+            return False
+        return True
+
+    def delete_config(self, config_id):
+        try:
+            config_id = str(config_id)
+            self.__cur.execute("call delete_seoes_config(%s)", (config_id,))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка удаления записи: {e}")
+            return False
+        return True
+    
+    def update_config(self, config_id, group_id, host_user_id, site_name, url, access_token):
+        try:
+            config_id = str(config_id)
+            self.__cur.execute("call update_seoes_config(%s, %s, %s, %s, %s, %s)", (
+                config_id, group_id, host_user_id, site_name, url, access_token))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка удаления записи: {e}")
+            return False
+        return True
+    
+    def create_search_data(self, config_id):
+        average_position = rnd.randint(1, 100)
+        click_rate = rnd.randint(100, 1000)
+        number_of_view = rnd.randint(100, 1000)
+        try:
+            config_id = str(config_id)
+            self.__cur.execute("call insert_seoes_search_data(%s, %s, %s, %s)", (
+                int(config_id), int(average_position), int(click_rate), int(number_of_view)))
+            #res = self.__cur.fetchall()
+            #print(res)
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка добавления записи: {e}")
+            self.__db.rollback()
+            return False
+        return True
+    
+    def delete_seoes_search_data(self, search_id):
+        try:
+            search_id = str(search_id)
+            self.__cur.execute("call delete_seoes_search_data(%s)", (search_id,))
+            self.__cur.connection.commit()
+        except Exception as e:
+            print(f"Ошибка удаления записи: {e}")
+            self.__db.rollback()
+            return False
+        return True
 
 
 def validate_data_users(role, name, number):
@@ -47,7 +142,6 @@ def validate_data_users(role, name, number):
 class Users:
     def __init__(self):
         self.users = []
-        self.users_id = []
 
     def write_in(self, database):
         self.users = []
@@ -60,4 +154,39 @@ class Users:
                     'user_number': user[3],
                 }
             )
-        print(self.users)
+
+
+class Users_Group_membership:
+    def __init__(self):
+        self.users_group = []
+
+
+def write_in_membership(Us_Gr, database):
+    Us_Gr.users_group = []
+    for membership in database.get_memberships():
+        Us_Gr.users_group.append([membership[0], membership[1]])
+    print(Us_Gr.users_group)
+
+
+class Configs:
+    def __init__(self):
+        self.configs_row = []
+
+
+class Search_data:
+    def __init__(self):
+        self.data_row = []
+
+
+def write_in_configs(configs, database):
+    configs.configs_row = []
+    for config in database.get_configs():
+        configs.configs_row.append(list(config))
+    print(configs.configs_row)
+
+
+def write_in_search_data(search_data, database, config_id):
+    search_data.data_row = []
+    for config in database.get_search_data(config_id):
+        search_data.data_row.append(list(config))
+    print(search_data.data_row)
--- seoes_main.py
+++ seoes_main.py
@@ -1,9 +1,11 @@
 import flask as f
 # Flask, render_template, g, request, redirect, url_for, session, flash
 import psycopg2
-from mystruct import FDataBase, Users, validate_data_users
+from mystruct import FDataBase, Users, validate_data_users, \
+    Users_Group_membership, write_in_membership, Configs, write_in_configs, \
+    Search_data, write_in_search_data
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, Sequence
+from sqlalchemy import Integer, String
 # import time
 
 
@@ -31,6 +33,9 @@ def connect_db():  # db.api
 db = connect_db()
 dbase = FDataBase(db)
 users = Users()
+memberships = Users_Group_membership()
+configs = Configs()
+search_data = Search_data()
 app = f.Flask(__name__)
 app.secret_key = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = \
@@ -41,7 +46,7 @@ db1 = SQLAlchemy(app)
 class Group(db1.Model):
     __tablename__ = 'seoes_groups'  # Указываем название таблицы
 
-    group_id = db1.Column(Integer, Sequence('group_id_seq'), primary_key=True)
+    group_id = db1.Column(Integer, primary_key=True)
     group_name = db1.Column(String(100), nullable=False)
 
     def __repr__(self):
@@ -49,50 +54,121 @@ class Group(db1.Model):
                     group_name='{self.group_name}')>"
 
 
-@app.route('/homepage')
+@app.route('/homepage', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def home_page():
     if 'user_id' not in f.session:
         return f.redirect(f.url_for('index'))
-    return f.render_template('homepage.html', user_id=f.session["user_id"],
+    write_in_membership(memberships, dbase)
+    write_in_configs(configs, dbase)
+    if f.request.method == 'POST':
+        group_id = f.request.form['group_id']
+        host_user_id = f.request.form['host_user_id']
+        site_name = f.request.form['site_name']
+        url = f.request.form['url']
+        access_token = f.request.form['access_token']
+        if dbase.create_config(group_id, host_user_id, site_name, url, access_token):  # Замените на вашу функцию добавления в БД
+            write_in_configs(configs, dbase)
+            return f.jsonify({'response': "good create user"}), 200
+        else:
+            return f.jsonify({'errors': "Ошибка добавления"}), 500
+    if f.request.method == 'DELETE':
+        id = f.request.form['config_id']
+        try:
+            if dbase.delete_config(id):
+                users.write_in(dbase)
+                return f.jsonify({'response': "good delete"}), 200
+            else:
+                return f.jsonify({'message': 'Ошибка удаления'}), 500
+        except ValueError:
+            return f.jsonify({'message': 'Неверный тип данных'}), 500
+    if f.request.method == 'PUT':
+        id = f.request.form['config_id']
+        group_id = f.request.form['group_id']
+        host_user_id = f.request.form['host_user_id']
+        site_name = f.request.form['site_name']
+        url = f.request.form['url']
+        access_token = f.request.form['access_token']
+        if dbase.update_config(id, group_id, host_user_id, site_name, url, access_token):  # Замените на вашу функцию добавления в БД
+            write_in_configs(configs, dbase)
+            return f.jsonify({'response': "good update user"}), 200
+        else:
+            return f.jsonify({'errors': "Ошибка обновления"}), 500
+    return f.render_template('homepage.html', user_id=int(f.session['user_id']),
+                             configs=configs.configs_row,
                              role_id=f.session['role_id'])
 
 
-@app.route('/groups')
+@app.route('/groups', methods=['GET', 'POST', 'DELETE'])
 def groups_page():
     if 'user_id' not in f.session:
         return f.redirect(f.url_for('index'))
     groups = Group.query.all()
-    print(groups)
     return f.render_template('groups.html', groups=groups,
+                             memberships=memberships,
+                             users=users.users,
                              user_id=f.session["user_id"],
                              role_id=f.session['role_id'])
 
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/homepage/<int:config_id>', methods=['GET', 'POST', 'DELETE'])
+def search_data_page(config_id):
+    if 'user_id' not in f.session:
+        return f.redirect(f.url_for('index'))
+    write_in_search_data(search_data, dbase, int(config_id))
+    if f.request.method == 'POST':
+        if dbase.create_search_data(config_id):  # Замените на вашу функцию добавления в БД
+            write_in_search_data(search_data, dbase, int(config_id))
+            return f.jsonify({'response': "good create user"}), 200
+        else:
+            write_in_search_data(search_data, dbase, int(config_id))
+            return f.jsonify({'errors': "Ошибка добавления"}), 500
+    if f.request.method == 'DELETE':
+        id = f.request.form['search_id']
+        try:
+            if dbase.delete_seoes_search_data(id):
+                write_in_search_data(search_data, dbase, int(config_id))
+                return f.jsonify({'response': "good delete"}), 200
+            else:
+                return f.jsonify({'message': 'Ошибка удаления'}), 500
+        except ValueError:
+            return f.jsonify({'message': 'Неверный тип данных'}), 500
+    return f.render_template('search_data_page.html',
+                             search_data=search_data.data_row,
+                             user_id=f.session["user_id"],
+                             role_id=f.session['role_id'], config_id=config_id)
+
+
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def users_page():
     if 'user_id' not in f.session:
+        print(2)
         return f.redirect(f.url_for('index'))
     if f.request.method == 'POST':
-        role = f.request.form['role']
-        name = f.request.form('f_name')
-        number = f.request.form('number')
+        # f.session['user_id'] = user_id
+        # f.session['role_id'] = user['role_id']
+        role = f.request.form['role_id']
+        name = f.request.form['user_full_name']
+        number = f.request.form['user_number']
         errors = validate_data_users(role, name, number)
         if errors:
-            return f.jsonify({'errors': errors}), 400
-        dbase.create_user(role, name, number)  # Замените на вашу функцию добавления в БД
-        users.write_in(dbase)
-        return f.jsonify({'message': 'Данные успешно добавлены'}), 201
+            return f.jsonify({'errors': errors}), 500
+        if dbase.create_user(role, name, number):  # Замените на вашу функцию добавления в БД
+            users.write_in(dbase)
+            return f.jsonify({'response': "good create user"}), 200
+        else:
+            print('ошибка добавления')
+    if f.request.method == 'DELETE':
+        id = f.request.form['Id']
+        try:
+            if dbase.delete_user(id):
+                users.write_in(dbase)
+                return f.jsonify({'response': "good delete"}), 200
+            else:
+                return f.jsonify({'message': 'Ошибка удаления'}), 500
+        except ValueError:
+            return f.jsonify({'message': 'Неверный тип данных'}), 500
+
     return f.render_template('users.html', users=users.users,
-                             user_id=f.session["user_id"],
-                             role_id=f.session['role_id'])
-
-
-@app.route('/search_data')
-def search_data_page():
-    if 'user_id' not in f.session:
-        return f.redirect(f.url_for('index'))
-    groups = Group.query.all()
-    return f.render_template('search_data.html', group=groups,
                              user_id=f.session["user_id"],
                              role_id=f.session['role_id'])
 
@@ -115,4 +191,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.31.179', port=5000)
+    app.run(debug=True, host='192.168.56.1', port=5000)
